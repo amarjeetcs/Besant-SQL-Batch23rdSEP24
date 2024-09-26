889ee09a1ed81f417dff40cd79d6166f20144d2f
Amarjeet Kumar Singh <amarjeetcs79@gmail.com>
2024-09-26 11:18:14 -0700

    minor changes

--- sql-server-commands-notes/AllSqlServerCommandsLists.sql
+++ sql-server-commands-notes/AllSqlServerCommandsLists.sql
@@ -1,11 +1,12 @@
-create database asr_training; -- for creating dataabse
+create database asr_training1; -- for creating dataabse
 SELECT name FROM sys.databases; -- for selecting database 
-use asr_training;  -- for using database
+use asr_new_databasename;  -- for using database
 SELECT name FROM sys.tables;   --for checking list of tables present in database
 create table student(id int, name varchar(20), city varchar(20)); --student table created
 EXEC sp_help 'student';  --for describing the table information
 insert into student(id, name, city) values(101,'amarjeet kumar singh','bangalore'); --inserted the data into table student
 select *From student; --feching data from tables
 
-
+drop database asr_training;
+ALTER DATABASE asr_training MODIFY NAME = asr_new_databasename;   -- change database name
 
